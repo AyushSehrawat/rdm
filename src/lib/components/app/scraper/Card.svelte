356316cfb024f0f1ed111c5af9ba8d1c6d05f155
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2023-11-08 14:48:53 +0530

    ui: mobile improvements on scraper

--- src/lib/components/app/scraper/Card.svelte
+++ src/lib/components/app/scraper/Card.svelte
@@ -8,13 +8,13 @@
 
 <a
 	href="/app/scraper/{type}/{imdbId}"
-	class="flex flex-col group justify-end gap-2 w-40 md:w-48 h-72 rounded-md bg-cover bg-center relative select-none"
+	class="flex flex-col group justify-end gap-2 w-36 md:w-48 h-72 rounded-md bg-cover bg-center relative select-none"
 	style="background-image: url({poster})"
 >
 	<div
 		class="bg-black bg-opacity-60 text-white p-2 absolute bottom-0 w-full opacity-0 group-hover:opacity-100 transition-opacity duration-200"
 	>
-		<p class="text-sm max-w-[10rem] md:max-w-[12rem]">{name}</p>
-		<p class="text-xs max-w-[10rem] md:max-w-[12rem]">{releaseInfo}</p>
+		<p class="text-sm max-w-[9rem] md:max-w-[12rem]">{name}</p>
+		<p class="text-xs max-w-[9rem] md:max-w-[12rem]">{releaseInfo}</p>
 	</div>
 </a>
