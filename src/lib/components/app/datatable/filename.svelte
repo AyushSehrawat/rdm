431656789e7ff471a779a74dc6b914be59ff3c1b
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2023-11-23 14:13:13 +0530

    feat: added some more badges and code improvements

--- src/lib/components/app/datatable/filename.svelte
+++ src/lib/components/app/datatable/filename.svelte
@@ -2,22 +2,38 @@
 	import { Badge } from '$lib/components/ui/badge';
 	import type { ParsedDownloadsResponse, ParsedTorrentsResponse } from '$lib/app/types';
 
-	export let filedata: ParsedDownloadsResponse | ParsedTorrentsResponse;
+	// TODO: add type
+	export let filedata: any;
+	const fileCount = filedata.links?.length || null;
 </script>
 
-{#if filedata.metadata?.parsedData?.title}
-	<p class="font-semibold">{filedata.metadata?.parsedData?.title}</p>
-{/if}
+<div class="flex flex-row gap-1 items-center flex-wrap">
+	{#if filedata.metadata?.parsedData?.title}
+		<p class="font-semibold">{filedata.metadata?.parsedData?.title}</p>
+	{:else}
+		<p class="font-semibold">{filedata.filename}</p>
+	{/if}
+
+	{#if fileCount}
+		<Badge variant="outline">
+			{fileCount}
+			{fileCount === 1 ? 'file' : 'files'}
+		</Badge>
+	{/if}
+	{#if filedata.metadata?.parsedData?.fullSeason}
+		<Badge variant="secondary">Full Season</Badge>
+	{/if}
+</div>
 <p class="text-xs text-muted-foreground">{filedata.filename}</p>
 <div class="flex gap-1 items-start flex-wrap">
-	{#if filedata.metadata?.parsedData?.seasons && filedata.metadata?.parsedData?.seasons.length > 0}
+	{#if filedata.metadata?.parsedData?.seasons?.length}
 		{#each filedata.metadata?.parsedData?.seasons as season}
 			<Badge variant="outline">
 				S{season}
 			</Badge>
 		{/each}
 	{/if}
-	{#if filedata.metadata?.parsedData?.episodeNumbers && filedata.metadata?.parsedData?.episodeNumbers.length > 0}
+	{#if filedata.metadata?.parsedData?.episodeNumbers?.length}
 		{#each filedata.metadata?.parsedData?.episodeNumbers as episode}
 			<Badge variant="outline">
 				E{episode}
@@ -38,8 +54,5 @@
 		{#each filedata.metadata?.parsedData?.languages as language}
 			<Badge variant="outline">{language}</Badge>
 		{/each}
-	{/if}
-	{#if filedata.metadata?.parsedData?.fullSeason && filedata.metadata?.parsedData?.fullSeason === true}
-		<Badge variant="outline">Full Season</Badge>
 	{/if}
 </div>
